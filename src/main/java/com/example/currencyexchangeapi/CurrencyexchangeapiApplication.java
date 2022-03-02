d02d6ade16db2bd2425573963e07e9dffb2688e0
jforce_serkana <serkanakagunduz@anadolusigorta.com.tr>
2022-03-02 09:42:28 +0300

    RestTemplate Bean deleted.

--- src/main/java/com/example/currencyexchangeapi/CurrencyexchangeapiApplication.java
+++ src/main/java/com/example/currencyexchangeapi/CurrencyexchangeapiApplication.java
@@ -3,11 +3,6 @@ package com.example.currencyexchangeapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -15,20 +10,6 @@ public class CurrencyexchangeapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyexchangeapiApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(getClientHttpRequestFactory()));
-		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-		return restTemplate;
-	}
-
-	private SimpleClientHttpRequestFactory getClientHttpRequestFactory() {
-		SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
-		clientHttpRequestFactory.setConnectTimeout(30000);
-		clientHttpRequestFactory.setReadTimeout(30000);
-		return clientHttpRequestFactory;
 	}
 
 }
